a6b778a8921a32f2b24ae2b2fce638b8405a6e29
Lorena <lorenazotaj10@gmail.com>
2024-05-23 15:31:11 +0200

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import './App.css'
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
         lg: `"nav nav" "aside main"`, // >1024px
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
