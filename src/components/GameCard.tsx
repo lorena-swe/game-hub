02f1fb3761c67cda354becabe63354e13ce95728
Lorena <lorenazotaj10@gmail.com>
2024-05-24 16:02:28 +0200

    Refactored GameCards sizing (and skeletons)

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden" minHeight={250}>
+    <Card>
       <Image
         src={getCroppedImageUrl(game.background_image)}
         height={150}
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card maxHeight="250px">
       <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
